bd9fe64221e19ca6b0b2a915f2e4dd4e49e5d1c3
tecKaleAnders <143652890+tecKaleAnders@users.noreply.github.com>
2024-06-17 10:27:23 -0500

    Initial Commit

---
+++ swiftUI navegacion/swiftUI_navegacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_navegacionApp.swift
+//  swiftUI navegacion
+//
+//  Created by David Acevedo on 17/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_navegacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
